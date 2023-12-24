d8cd84a88fbc841377f892f37c9d3e690c5cfc94
yuri00kid <tahasarmadifar81@gmail.com>
2023-12-24 16:54:19 +0330

    changes

--- PersonalInfo/Program.cs
+++ PersonalInfo/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Name of Persons
+
+string firstPersonName = "Liam";
+string secondPersonName = "Richard";
+string thirdPersonName = "Adriana";
+
+// Age of Persons
+
+int firstPersonAge = 18;
+int secondPersonAge = 56;
+int thirdPersonAge = 42;
+
+// Phone numbers
+
+decimal firstPhoneNumber = +15469879;
+decimal secondPhoneNumber = +127586545;
+decimal thirdPhoneNumber = +15469872;
+
+// Print all the Informations
+
+Console.WriteLine("Name: " + firstPersonName + "  Age: " + firstPersonAge + "  PhoneNumber: " + firstPhoneNumber);
+Console.WriteLine("Name: " + secondPersonName + "  Age: " + secondPersonAge + "  PhoneNumber: " + secondPhoneNumber);
+Console.WriteLine("Name: " + thirdPersonName + "  Age: " + thirdPersonAge + "  PhoneNumber: " + thirdPhoneNumber);
+
+
+

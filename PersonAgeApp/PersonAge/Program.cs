e8ad1d8ad3771cd0c0a430558f43bc0403eac84c
yuri00kid <tahasarmadifar81@gmail.com>
2023-12-27 17:42:41 +0330

    Some useless apps

---
+++ PersonAgeApp/PersonAge/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ask for the Age
+
+Console.Write("How old are you? : ");
+string? ageString = Console.ReadLine();
+
+// Converting ageString to an int number 
+
+bool isValid = int.TryParse(ageString, out int age);
+Console.WriteLine($"Is this Valid? {isValid}. The number was: {age}");
+
+// Doing the math thing
+int ageAfter = age + 25;
+Console.WriteLine($"After 25 years you will be {ageAfter} years old");
+
+int ageEarlier = age - 25;
+Console.WriteLine($"25 years Earlier you was at {ageEarlier} years old");
+
+
+    

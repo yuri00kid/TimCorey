f60cc894f1d0563df4f4f22a1e6c008839e3d23b
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-09 22:04:50 +0330

    an other useless app

---
+++ IfStatementDemoApp/IfStatementDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.Write("What is your name:");
+string? firsName = Console.ReadLine();
+
+if (firsName.ToLower() == "tim")
+{
+    Console.WriteLine("Welcome Professor.");
+}
+else
+{
+    Console.WriteLine("Welcome Student.");
+}
+

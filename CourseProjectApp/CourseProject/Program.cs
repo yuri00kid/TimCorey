7f5c2ddd11757fb2e376bdc6a55c97d6a3303f65
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-16 05:31:13 +0330

    again , you know it!

---
+++ CourseProjectApp/CourseProject/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.Write("What is your name: ");
+string? firstName = Console.ReadLine();
+
+if (firstName.ToLower() == "bob" ||
+    firstName.ToLower() == "sue")
+{
+    Console.WriteLine("Hello Professor.");
+}
+else
+{
+    Console.WriteLine("Hello Student.");
+}
+
+Console.Write("How old are you: ");
+string? ageText = Console.ReadLine();
+
+int.TryParse(ageText, out int age);
+
+if (age < 21)
+{
+    int waitTime = 21 - age;
+    Console.WriteLine($"You must to wait {waitTime} years for this course.");
+}
+else
+{
+    Console.WriteLine($"Your age is {age}");
+}

2fbdf9ee5321a1f90facc6d9c7a4127598119698
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-18 01:29:59 +0330

    somthing useless again

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("After you finish , Type (done)");
+Console.WriteLine("Please add Name of Students: ");
+
+
+List<string> firstNames = new List<string>();
+string name;
+int count = 0;
+do
+{
+    name = Console.ReadLine();
+    firstNames.Add(name);
+    count++;
+
+} while (name.ToLower() != "done");
+
+
+Console.WriteLine($"You type {count} Names .");

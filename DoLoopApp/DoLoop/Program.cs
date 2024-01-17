eb10ebcc1810750d8b0b48fb3429474c212a8f05
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-17 22:20:33 +0330

    just for training

---
+++ DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿string firstName;
+Console.WriteLine("For quit , please type (exit)");
+
+do
+{
+    Console.Write("What is your Name: ");
+    firstName = Console.ReadLine();
+
+    if (firstName.ToLower() == "tim")
+    {
+        Console.WriteLine("Welcome Professor Corey.");
+    }
+    else if (firstName.ToLower() == "exit")
+    {
+        Console.WriteLine("End of the Program.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome Student.");
+    }
+    
+} while (firstName.ToLower() != "exit") ;
+

ab2bc32a7a9332be2f779fcc868325cf080ea693
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-21 08:42:32 +0330

    For Loops Demo

---
+++ ForLoopApp/ForLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.Write("Please type a couple Names with format of comma saparated (***,***,***): ");
+string firstNames = Console.ReadLine();
+
+List<string> listNames = firstNames.Split(',').ToList();
+
+for (int i = 0; i < listNames.Count; i++)
+{
+    Console.WriteLine($"Hello {listNames[i]}");
+}
+
+
+

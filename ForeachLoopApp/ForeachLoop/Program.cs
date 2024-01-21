836da39b4d202542db47a52bba4d75f15fc5597b
yuri00kid <tahasarmadifar81@gmail.com>
2024-01-21 09:02:18 +0330

    Foreach Loops

---
+++ ForeachLoopApp/ForeachLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Finish = done");
+
+Console.Write("Give us The Names: ");
+string firstNames;
+List<string> listNames = new();
+do
+{
+    firstNames =  Console.ReadLine();
+    listNames.Add(firstNames);
+
+} while (firstNames.ToLower() != "done");
+
+foreach (string firstName in listNames)
+{
+    Console.WriteLine($"Hello {firstName}");
+}
+
